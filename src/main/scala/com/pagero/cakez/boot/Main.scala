ed272633be87911d9e385680698e0ab032e1c004
eranga herath <erangaeb@gmail.com>
2016-01-29 21:19:37 +0530

    tweak

--- src/main/scala/com/pagero/cakez/boot/Main.scala
+++ src/main/scala/com/pagero/cakez/boot/Main.scala
@@ -2,11 +2,16 @@ package com.pagero.cakez.boot
 
 import akka.actor.Props
 import com.pagero.cakez.actors.{CakezActorSystem, InitReader, InputReader}
+import org.slf4j.LoggerFactory
 
 /**
  * Created by eranga on 1/28/16.
  */
 object Main extends App with CakezActorSystem {
+  def logger = LoggerFactory.getLogger(this.getClass)
+
+  logger.debug("Booting application")
+
   // start senz sender
   val senzSender = system.actorOf(Props(classOf[InputReader]), name = "InputRender")
   senzSender ! InitReader
