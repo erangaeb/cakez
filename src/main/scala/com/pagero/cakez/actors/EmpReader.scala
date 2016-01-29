868e2578658da01c5f91d56db12f120a89178145
eranga herath <erangaeb@gmail.com>
2016-01-29 12:50:04 +0530

    employee reader

---
+++ src/main/scala/com/pagero/cakez/actors/EmpReader.scala
@@ -0,0 +1,41 @@
+package com.pagero.cakez.actors
+
+import akka.actor.Actor
+import com.pagero.cakez.db.CassandraCluster
+import com.pagero.cakez.handlers.UserHandler
+import com.pagero.cakez.services.{CassandraUserDbCompImpl, SprayUserServiceCompImpl}
+
+case class InitReader()
+
+/**
+ * Created by eranga on 1/9/16.
+ */
+class EmpReader extends Actor {
+
+  override def preStart = {
+    println("----started----- " + context.self.path)
+  }
+
+  // employee handler dependencies
+  trait EmployeeHandlerConfig extends SprayUserServiceCompImpl with CassandraUserDbCompImpl with CassandraCluster
+
+  val employeeHandler = new UserHandler with EmployeeHandlerConfig
+
+  override def receive: Receive = {
+    case InitReader => {
+      // listen for user inputs via commandline
+      while (true) {
+        println("--------------------------------------------")
+        println("#Employee(id, name, department)")
+        println("--------------------------------------------")
+
+        val inputEmployee = scala.io.StdIn.readLine()
+
+        if (!inputEmployee.isEmpty) {
+          // handle employee via Employee handler
+          employeeHandler.createUser()
+        }
+      }
+    }
+  }
+}
--- src/main/scala/com/pagero/cakez/boot/Main.scala
+++ src/main/scala/com/pagero/cakez/boot/Main.scala
@@ -1,24 +1,17 @@
 package com.pagero.cakez.boot
 
-import com.pagero.cakez.db.CassandraCluster
-import com.pagero.cakez.handlers.UserHandler
-import com.pagero.cakez.services.{CassandraUserDbCompImpl, SprayUserServiceCompImpl}
+import akka.actor.{ActorSystem, Props}
+import com.pagero.cakez.actors.{EmpReader, InitReader}
 
 /**
  * Created by eranga on 1/28/16.
  */
 object Main extends App {
 
-  // default usage
-  trait DefaultConfig extends SprayUserServiceCompImpl with CassandraUserDbCompImpl with CassandraCluster
+  // start actor to read inputs
+  implicit val system = ActorSystem("cakez")
 
-  val defaultUserHandler = new UserHandler with DefaultConfig
-  defaultUserHandler.createUser()
-  defaultUserHandler.getUser(1)
-
-//  // test usage
-//  trait TestConfig extends SprayUserServiceCompImpl with CassandraUserDbCompImpl with CassandraCluster
-//
-//  val testUserHandler = new UserHandler with TestConfig
-//  testUserHandler.getUser(1)
+  // start senz sender
+  val senzSender = system.actorOf(Props(classOf[EmpReader]), name = "EmpRender")
+  senzSender ! InitReader
 }
